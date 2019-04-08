a2f9fe70691d24f833d5ed88b93dc271ba6b1f9a
Luke Horgan <horgan.l@husky.neu.edu>
2019-04-08 17:20:14 -0400

    start working on backlight thing

--- Backlight.h
+++ Backlight.h
@@ -1,6 +1,17 @@
+#include <chrono>
+#include <ctime>
+#include <stdlib.h> 
+#include <atomic>
+
 class Backlight {
 public:
     Backlight() {
 
     }
+
+private:
+    double refreshRate; // fps
+    double refreshNoise; // also fps (ie +/- 0.1fps)
+    std::atomic<bool> white; // is the frame black or white?
+    std::atomic<bool> startFrame; // should the motor be home at the start of this frame?
 };

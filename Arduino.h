0b8d2177f7ace003052a9abc2253760cb39ebd98
Luke Horgan <horgan.l@husky.neu.edu>
2019-04-09 00:56:36 -0400

    simulate micros function

--- Arduino.h
+++ Arduino.h
@@ -15,7 +15,9 @@ public:
     Arduino() {
         this->motor.reset(new Motor(10, 0.01, 0, 1.0));
         this->backlight.reset(new Backlight(240, 0));
-        
+
+        start = std::chrono::high_resolution_clock::now();
+
         setup();
 
         running = true;
@@ -31,17 +33,21 @@ private:
     std::unique_ptr<Backlight> backlight;
     std::unique_ptr<Motor> motor;
     bool running;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+
+    unsigned long long micros() { // simulated Arduino micros function
+        auto elapsed = std::chrono::high_resolution_clock::now() - start;
+        return std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+    }
 
     void run() {
         while(running) {
             loop();
             std::this_thread::sleep_for(std::chrono::nanoseconds(63)); // Arduino runs at 12.5Mhz
         }
-    }
-    
+    }    
 
     void setup() {
-
     }
 
     void loop() {
